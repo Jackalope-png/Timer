9feaeb91e93d7d10bc80d8c85198c0a4e09fadf6
Jackalope <your.email@example.com>
2024-10-24 17:49:52 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,47 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useState, useEffect } from "react"
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const [data, setData] = useState(0)
+const [isRunning, setIsRunning] = useState(true)
+const [count, setcount] = useState(100)
+useEffect(() =>{
+if (!isRunning) return
+const timer = setInterval(() => {
+  setData(prevData => prevData + 1);
+}, 1000);
+return () => clearInterval(timer);
+},[isRunning])
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+useEffect(() =>{
+  if (!isRunning) return
+  const timer = setInterval(() => {
+    setcount(prevCount => (prevCount > 0 ? prevCount - 1 : 0))
+  }, 1000);
+  return () => clearInterval(timer);
+  },[isRunning])
+
+const formatTime = (sec) => {
+  const minutes = Math.floor(sec / 60);
+  const remainingSeconds = sec % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+};
+
+const theEnd = () =>{
+  setIsRunning(false);
+}
+const reset = ( ) =>{
+  setData(0)
+}
+const minutes = 0
+return(
+  <div>
+  <div>Seconds:{data}</div>
+  <div>Timer: {formatTime(data)}</div>
+  <div>countdown {count} </div>
+  <div>countdown {formatTime(count)} </div>
+  <div onClick={theEnd}>Stop</div>
+  <div onClick={reset}>reset</div>
+  </div>
+ )
 }
